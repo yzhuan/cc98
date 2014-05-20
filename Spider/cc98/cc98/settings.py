ac4c91612673ae5c5b1bd5e42323d1f94d9ea3c3
yzh <yzh@yzh-PC.(none)>
2014-05-20 07:02:43 -0400

    i# On branch master 	new file:   Spider/cc98/cc98/__init__.py 	new file:   Spider/cc98/cc98/items.py 	new file:   Spider/cc98/cc98/pipelines.py 	new file:   Spider/cc98/cc98/settings.py 	new file:   Spider/cc98/cc98/spiders/__init__.py 	new file:   Spider/cc98/scrapy.cfg

---
+++ Spider/cc98/cc98/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for cc98 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cc98'
+
+SPIDER_MODULES = ['cc98.spiders']
+NEWSPIDER_MODULE = 'cc98.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cc98 (+http://www.yourdomain.com)'
